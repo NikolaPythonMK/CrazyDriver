76fcbb45268e994fa1f97a13d7fdcc420e937b80
NikolaPythonMK <nikola.python159@gmail.com>
2022-06-24 17:49:16 +0200

    modified

--- CrazyDriver/Car.cs
+++ CrazyDriver/Car.cs
@@ -12,6 +12,8 @@ namespace CrazyDriver
         public Bitmap carModel { get; set; }
         public decimal horsePower { get; set; }
         public decimal speed { get; set; }
+        //
+
 
         public Car(String path, decimal horsePower)
         {
